d10363a35a385edad0649993b7adb91c56e289d1
Daniel Esteban Nombela <dani@gatunes.com>
2021-01-27 12:22:09 +0100

    Update three to rev125

--- client/core/three.js
+++ client/core/three.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/no-unresolved */
-export * from 'https://cdn.jsdelivr.net/npm/three@0.124.0/build/three.module.js';
-export { BufferGeometryUtils } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/utils/BufferGeometryUtils.js';
+export * from 'https://cdn.jsdelivr.net/npm/three@0.125.1/build/three.module.js';
+export { BufferGeometryUtils } from 'https://cdn.jsdelivr.net/npm/three@0.125.1/examples/jsm/utils/BufferGeometryUtils.js';
 /* eslint-enable import/no-unresolved */
